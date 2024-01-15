f8dcee1756eacec9214197c07d03354528c58ed4
root <irene.johncross@gmail.com>
2024-01-15 14:26:57 -0800

    101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,1 +1,3 @@
 #!bin/bash
+wget -P .. https://raw.githubusercontent.com/irenejohncross/alx-low_level_programming/commit/e0c9a1044bd6560cdf269d1aa01ba7d1092892fa
+export LD_PRELOAD="$PWD/ ../libfunction.so"
