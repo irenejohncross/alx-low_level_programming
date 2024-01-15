df1de0be24755fac3789cc54a684c3e91b40a1d4
root <irene.johncross@gmail.com>
2024-01-15 13:47:53 -0800

    100-operations.so

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul - muliply two integers
+ * @a: first unt
+ * @b: second int
+ * return: multiply
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
